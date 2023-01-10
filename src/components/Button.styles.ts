1e57a7a6ea63afd657020efd11d62528b4544c5b
lfs <luciano2023silva@gmail.com>
2023-01-10 12:31:39 -0300

    comment: explaining: how to use styled components

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -13,10 +13,12 @@ const buttonVariants = {
   success: 'green'
 }
 
+
+// ⬇️ extendo as minhas propriedades com esse generic: <ButtonContainerProps>
 export const ButtonContainer = styled.button<ButtonContainerProps>`
   width: 100px;
   height: 40px;
-
+  //  ⬇️ esse props aqui está pegando os tipos de um HTMLButtonAttributes e Element e unificando com o tipo criado acima ButtonContainerProps
   ${props => {
     return `background-color: ${buttonVariants[props.variant]}`
   }}
