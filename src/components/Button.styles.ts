249eb7a05d83c7c7061a178c642e8a6efa81a316
lfs <luciano2023silva@gmail.com>
2023-01-10 12:34:44 -0300

    fix: add {css} from styled-components

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -1,4 +1,4 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 
 export type ButtonVariant = 'primary' | 'secondary' | 'danger' | 'success';
 
@@ -20,6 +20,6 @@ export const ButtonContainer = styled.button<ButtonContainerProps>`
   height: 40px;
   //  ⬇️ esse props aqui está pegando os tipos de um HTMLButtonAttributes e Element e unificando com o tipo criado acima ButtonContainerProps
   ${props => {
-    return `background-color: ${buttonVariants[props.variant]}`
+    return css`background-color: ${buttonVariants[props.variant]}`
   }}
 `;
