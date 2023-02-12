239ed50be981885680fb2c789e96dd8d346905ca
lfs <luciano2023silva@gmail.com>
2023-02-12 19:36:03 -0300

    feat: explains Outlet component how his works

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -1,12 +1,20 @@
 import { Routes, Route } from 'react-router-dom'
 import { Home } from '../pages/Home'
 import { History } from '../pages/History'
+import { DefaultLayout } from '../layouts/DefaultLayout'
+import { AdminLayout } from '../layouts/AdminLayout'
 
 export function Router() {
   return (
     <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/history" element={<History />} />
+      <Route path="/" element={<DefaultLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/history" element={<History />} />
+      </Route>
+      <Route path="/admin" element={<AdminLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/history" element={<History />} />
+      </Route>
     </Routes>
   )
 }
