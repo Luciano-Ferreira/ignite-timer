8770bb71cbd0e871f1a363bfc4258ae6ef7f74a9
lfs <luciano2023silva@gmail.com>
2023-02-12 20:22:31 -0300

    feat: body and styles of Header fix: Routes

--- src/components/Router.tsx
+++ src/components/Router.tsx
@@ -2,16 +2,11 @@ import { Routes, Route } from 'react-router-dom'
 import { Home } from '../pages/Home'
 import { History } from '../pages/History'
 import { DefaultLayout } from '../layouts/DefaultLayout'
-import { AdminLayout } from '../layouts/AdminLayout'
 
 export function Router() {
   return (
     <Routes>
       <Route path="/" element={<DefaultLayout />}>
-        <Route path="/" element={<Home />} />
-        <Route path="/history" element={<History />} />
-      </Route>
-      <Route path="/admin" element={<AdminLayout />}>
         <Route path="/" element={<Home />} />
         <Route path="/history" element={<History />} />
       </Route>
